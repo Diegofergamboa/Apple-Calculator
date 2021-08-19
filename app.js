a742e067179adad59503be022840ff6ff920911b
Diegofergamboa <diegofergamboa@hotmail.es>
2021-08-19 17:37:50 -0500

    Function filterAction to deliver the value of the event to every function

--- app.js
+++ app.js
@@ -3,6 +3,23 @@
 // Necesidad de llamar los elementos por evento
 
 function getContentClick(event) {
-    const value = event.target.innerHTML ;
+    const value = event.target.innerHTML ; //Lo tome al llamar al evento directamente
+    filterAction(value);
+}
+
+// Función para distribuir el valor de los elementos y, realizar acciones con ellos.
+
+const filterAction = value => {
+    console.log('Estamos en FilterAction');
     console.log(value);
 }
+
+// Función para escribir los numeros en el output type.
+
+// Función para borrar.
+
+// Función para sumar.
+
+// Función para restar.
+
+// Función para dividir.
