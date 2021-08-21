c0ec82e6984eb32e25b77dfd9c44ed67b2bd864f
Diegofergamboa <diegofergamboa@hotmail.es>
2021-08-21 12:12:08 -0500

    Added the use of "," and the erase of the 0 when the input is a different int from zero

--- app.js
+++ app.js
@@ -25,15 +25,21 @@ const filterAction = value => {
     value === '7' ? addNumberInput(7) : null ;
     value === '8' ? addNumberInput(8) : null ;
     value === '9' ? addNumberInput(9) : null ; 
+    value === ',' ? addNumberInput(',') : null ;
 }
 
 // Función para hacer en el Input el print de el valor que se trajo por evento a través del HTMLinner
 function addNumberInput(value) {
     const screenValue = document.getElementsByClassName('calculator__screen')[0]; // El cero me dice la posicion que averigue haciendo primero el document.getElementbyClassName y me decía que era cero donde el valor del input
+    const screenPrint = screenValue.value;
+    // Función para que se borre el cero y si es una coma agregue el cero.
+    if (screenPrint === '0' && screenPrint.length === 1 && value !== ',') {
+        screenValue.value = value;
+        return ; // El return es para que cuando termine de hacer la validación del primer valor que se ingresa como evento
+        // termine la validación con la función llamada es decir con addNumberInput y, lo agregue.
+    }
     // Script para que no se borre sino que se agregue el siguiente numero
-    const screenPrint = screenValue.value += value ;
-    // screenValue.value = value ;
-    return screenPrint;
+    screenValue.value = screenPrint + value ;
 }
 
 
