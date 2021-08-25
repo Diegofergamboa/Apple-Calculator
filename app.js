a0ffd4b6fb5b0774c608c0d9d6ccb1a1663c19ba
Diegofergamboa <diegofergamboa@hotmail.es>
2021-08-25 16:36:10 -0500

    printing in the input_screen the value from all the integer. DONE!

--- app.js
+++ app.js
@@ -2,17 +2,41 @@
 
 function getContentClick(event) {
     const value = event.target.innerHTML; //1. Acceder al valor del evento.
+    filterAction(value);
     // Fin
 }
 
-Redireccionar los valores de cada inner html a una función en específico
+// Redireccionar los valores de cada inner html a una función en específico
 const filterAction = value => { 
+    // """Redireccionar cada uno de los botones por el valor del innerhtml, y redireccionarlos a cada función o problema por resolver en específico."""
+    
     value === '1' ? addNumberInteger(1) : null ;
+    value === '2' ? addNumberInteger(2) : null ;
+    value === '3' ? addNumberInteger(3) : null ;
+    value === '4' ? addNumberInteger(4) : null ;
+    value === '5' ? addNumberInteger(5) : null ;
+    value === '6' ? addNumberInteger(6) : null ;
+    value === '7' ? addNumberInteger(7) : null ;
+    value === '8' ? addNumberInteger(8) : null ;
+    value === '9' ? addNumberInteger(9) : null ;
+    value === '0' ? addNumberInteger(0) : null ;
+
+    // OPERACIONES
+    // value === '1' ? addNumberInteger(1) : null ;
+    // value === '1' ? addNumberInteger(1) : null ;
+    // value === '1' ? addNumberInteger(1) : null ;
 }
 
+
 function addNumberInteger (value) {
-    const inputScreen = document.getElementsByClassName('screen__calculator')[0];
-    inputScreen.value = '66';
+    // console.log('Dentro de AddnumberInteger');
+    // Colocar en el screen el valor que mandé del innerhtml en pantalla
+    const inputScreen = document.getElementsByClassName('calculator__screen')[0];
+    // const inputValue = inputScreen.value ;
+    inputScreen.value = value ;
+    // Necsito que no se me borre 
+    // console.log(inputValue);
+    // Colocar en el screen el restante de cada operación y, colocar la operación hasta el momento que se ha realizado
 } 
 
 
